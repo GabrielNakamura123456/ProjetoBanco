14c99f3abcdcd9d67974593f896d83b68a8b1867
Gabriel Nakamura Ogata <RM560671@fiap.com.br>
2026-05-06 11:03:05 -0300

    finaliza fluxo completo de contratacoes com rabbitmq

--- ProjetoBanco.Api/Program.cs
+++ ProjetoBanco.Api/Program.cs
@@ -1,12 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using ProjetoBanco.Api.Consumers;
 using ProjetoBanco.Api.Data;
+using ProjetoBanco.Api.Enums;
 using ProjetoBanco.Api.Messaging;
+using ProjetoBanco.Api.Models;
 using ProjetoBanco.Api.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,6 +26,31 @@ builder.Services.AddSingleton<RabbitMqPublisher>();
 builder.Services.AddHostedService<ContratacaoConsumer>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (db.Produtos.FirstOrDefault() is null)
+    {
+        db.Produtos.Add(new Emprestimo
+        {
+            Nome = "Empréstimo Pessoal",
+            TipoProduto = TipoProduto.Emprestimo,
+            ValorMaximo = 50000,
+            TaxaJuros = 2.5m
+        });
+
+        db.Produtos.Add(new MaquinaDeCartao
+        {
+            Nome = "Máquina de Cartão",
+            TipoProduto = TipoProduto.MaquinaDeCartao,
+            TaxaMdr = 2.99m
+        });
+
+        db.SaveChanges();
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
